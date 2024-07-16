83f3375bd56ea6dabdf1bd53d3a7da0aa9aaec9d
Sottim <santosottim@gmail.com>
2024-07-16 13:46:51 +0530

    Hyperparameter tuning on the models.

--- StudentPerformanceAnalysis-EndToEnd-Project/src/utils.py
+++ StudentPerformanceAnalysis-EndToEnd-Project/src/utils.py
@@ -7,6 +7,8 @@ import dill
 import pickle
 
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
+
 from src.exception import CustomException
 
 def save_object(file_path, obj):
@@ -22,12 +24,18 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
     
 
-def evaluate_models(X_train, y_train,X_test,y_test,models):
+def evaluate_models(X_train, y_train,X_test,y_test,models, params):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para=params[list(models.keys())[i]]
+            #Find the best parameters for the particular model with cross-validation of 3
+            gs = GridSearchCV(model,para,cv=3, n_jobs=-1)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_) #apply the best parameters for the particular model
             model.fit(X_train,y_train)
 
             y_train_pred = model.predict(X_train)
